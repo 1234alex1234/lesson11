88fecd70fa949ef19ebd3a57af6124a9de637d53
1234alex1234 <verygoog@yandex.ru>
2022-01-31 12:06:51 +0300

    Исправил дз номер 2

--- script.js
+++ script.js
@@ -11,12 +11,9 @@ console.log(typeof adaptive);
 
 console.log(screens.length);
 
-console.log(`"Стоимость верстки экранов ${screenPrice} рублей" и
- "Стоимость разработки сайта ${fullPrice} рублей"`);
+console.log(`Стоимость верстки экранов ${screenPrice} рублей и
+ Стоимость разработки сайта ${fullPrice} рублей`);
 console.log(screens.toLowerCase().split(", "));
 console.log(fullPrice * (rollback/100));
 
-alert('Привет, Сергей!');
-
-console.log('Как у Вас дела');
 
