6eeeb52a3720281d9f3e753d52ee3ecafbd896c3
1234alex1234 <verygoog@yandex.ru>
2022-02-01 21:40:43 +0300

    Домашнее задание номер 3

--- script.js
+++ script.js
@@ -1,19 +1,35 @@
-const title = "Калькулятор";
-const screens = "Простые, Сложные, Интерактивные";
-let screenPrice = 45;
-let rollback = 55;
-let fullPrice = 1000000;
-let adaptive = true;
+let rollback = 5;
+let title = prompt("Как называется наш проект");
+let screens = prompt("Какие типы экранов нужно разработать");
+let screenPrice = +prompt("Сколько будет стоить данная работа");
+let adaptive = !!prompt("Нужен ли адаптив на сайте");
+
+let service1 = prompt("Какой дополнительный вид услуг нужен");
+let servicePrice1 = +prompt("Сколько это будет стоить");
+let service2 = prompt("Какой дополнительный вид услуг нужен");
+let servicePrice2 = +prompt("Сколько это будет стоить");
+
+let fullPrice = screenPrice + servicePrice1 + servicePrice2;
+let servicePercentPrice = fullPrice - fullPrice * (rollback/100);
+
+if (fullPrice >= 30000) {
+  console.log("Даем скидку 10%");
+} else if (fullPrice < 30000 && fullPrice >= 15000) {
+  console.log("Даем скидку 5%");
+} else if (fullPrice >= 0 && fullPrice < 15000) {
+  console.log("Скидка не предусмотрена");
+} else {
+  console.log("Что-то пошло не так!");
+}
 
 console.log(typeof title);
 console.log(typeof fullPrice);
 console.log(typeof adaptive);
-
 console.log(screens.length);
-
-console.log(`Стоимость верстки экранов ${screenPrice} рублей и
- Стоимость разработки сайта ${fullPrice} рублей`);
+console.log(`Стоимость верстки экранов ${screenPrice} рублей 
+и Стоимость разработки сайта ${fullPrice} рублей`);
 console.log(screens.toLowerCase().split(", "));
 console.log(fullPrice * (rollback/100));
 
-
+console.log(fullPrice);
+console.log(Math.ceil(servicePercentPrice));
