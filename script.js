8ca2f77864d04ddf050d43a568c8ae9fd74582ab
1234alex1234 <verygoog@yandex.ru>
2022-02-06 22:00:20 +0300

    Домашнее задание номер 5

--- script.js
+++ script.js
@@ -1,23 +1,47 @@
 //Переменные
 let rollback = 5;
 
-let title = prompt("Как называется наш проект");
-let screens = prompt("Какие типы экранов нужно разработать");
-let screenPrice = +prompt("Сколько будет стоить данная работа");
+let title = prompt("Как называется наш проект", "Калькулятор верстки");
+let screens = prompt("Какие типы экранов нужно разработать", "Простые, Сложные, Интерактивные");
+let screenPrice;
 let adaptive = !!confirm("Нужен ли адаптив на сайте");
 
-let service1 = prompt("Какой дополнительный вид услуг нужен");
-let servicePrice1 = +prompt("Сколько это будет стоить");
-let service2 = prompt("Какой дополнительный вид услуг нужен");
-let servicePrice2 = +prompt("Сколько это будет стоить");
+let service1;
+let servicePrice1;
+let service2;
+let servicePrice2;
+
 let fullPrice;
 let allServicePrices;
 let servicePercentPrice;
 
 
+const isNumber = function(num) {
+   return !isNaN(parseFloat(num)) && isFinite(num);
+ };
+
 const getAllServicePrices = function() {
-  return servicePrice1 + servicePrice2;
+ for (let i = 0; i < 2; i++) {
+     if(i === 0) {
+         service1 = prompt("Какой дополнительный вид услуг нужен", "Cms");
+         servicePrice1 = prompt("Сколько это будет стоить");
+        while(!isNumber(servicePrice1)) {
+          alert("Введите пожалуйста число!");
+          servicePrice1 = prompt("Сколько это будет стоить");
+        }
+     }  else if(i === 1) {
+         service2 = prompt("Какой дополнительный вид услуг нужен", "Метрика");
+         servicePrice2 = prompt("Сколько это будет стоить");
+        while(!isNumber(servicePrice2)) {
+          alert("Введите пожалуйста число!");
+          servicePrice2 = prompt("Сколько это будет стоить");
+        }
+     }
+   }
+  
+   return Number(servicePrice1) + Number(servicePrice2);
 };
+
 
 function getFullPrice() {
   fullPrice = screenPrice + allServicePrices;
@@ -50,6 +74,12 @@ function getRollbackMassage() {
   }
 }
 
+screenPrice = 0;
+let i = 0;
+do {
+  screenPrice += +prompt("Сколько будет стоить данная работа", 20000);
+  i++;
+} while (i < 1);
 
 allServicePrices = getAllServicePrices();
 fullPrice = getFullPrice();
@@ -59,6 +89,10 @@ showTypeOf(title);
 showTypeOf(fullPrice);
 showTypeOf(adaptive);
 
+console.log(allServicePrices);
+console.log(screenPrice);
 console.log(screens.toLowerCase().split(", "));
 console.log(getRollbackMassage());
 console.log(servicePercentPrice);
+
+
